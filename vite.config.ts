e95331c9e1f8ab11ce6230619645686e87c02e69
seveibar <seveibar@gmail.com>
2024-12-14 22:42:36 -0800

    make it easy to debug bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { resolve } from "node:path"
 import winterspecBundle from "@tscircuit/file-server/dist/bundle"
 import { getNodeHandler } from "winterspec/adapters/node"
+import { visualizer } from "rollup-plugin-visualizer"
 
 const fakeHandler = getNodeHandler(winterspecBundle as any, {})
 
@@ -24,13 +25,21 @@ function apiServerPlugin(): Plugin {
 
 const plugins: any[] = [react()]
 
-if (!process.env.VERCEL) {
+if (!process.env.VERCEL && !process.env.STANDALONE) {
   plugins.push(apiServerPlugin())
 }
 
 let build: any = undefined
 
 if (process.env.STANDALONE) {
+  // plugins.push(
+  //   visualizer({
+  //     filename: "stats.html",
+  //     open: true, // Will open the stats.html file after build
+  //     gzipSize: true,
+  //     brotliSize: true,
+  //   }),
+  // )
   build = {
     lib: {
       entry: resolve(__dirname, "src/main.tsx"),
@@ -39,6 +48,7 @@ if (process.env.STANDALONE) {
       formats: ["umd"],
     },
     minify: true,
+    // reportCompressedSize: true,
   }
 }
 

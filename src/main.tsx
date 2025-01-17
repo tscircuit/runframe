273385b60865a4e8722b3244a5c47c92ed33dde9
B-Mustafa <mustafabhikhpurwala@gmail.com>
2025-01-17 16:51:25 +0530

    formatted the code

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import { RunFrameForCli } from "lib/runner"
-import React from "react"
-import { createRoot } from "react-dom/client"
+import { RunFrameForCli } from "lib/runner";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("root")!)
+const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <div style={{ width: "100vw", height: "100vh", display: "flex", flexDirection: "column" }}>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <RunFrameForCli debug={window.location.search.includes("debug")} />
     </div>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

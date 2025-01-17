9dea9d9cc096f791f2e81e4a27a3f5565155ea85
Arnav K <arnavkaushal09@gmail.com>
2025-01-17 23:20:26 +0000

    update tabs

--- lib/components/ui/tabs.tsx
+++ lib/components/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "rf-inline-flex rf-h-10 rf-items-center rf-justify-center rf-rounded-md rf-bg-slate-100 rf-p-1 rf-text-slate-500 dark:rf-bg-slate-800 dark:rf-text-slate-400",
+      "rf-inline-flex rf-h-9 rf-items-center rf-justify-center rf-rounded-lg rf-bg-slate-100 rf-p-1 rf-text-slate-500 dark:rf-bg-slate-800 dark:rf-text-slate-400",
       className,
     )}
     {...props}
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "rf-inline-flex rf-items-center rf-justify-center rf-whitespace-nowrap rf-rounded-sm rf-px-3 rf-py-1.5 rf-text-sm rf-font-medium rf-ring-offset-white rf-transition-all focus-visible:rf-outline-none focus-visible:rf-ring-2 focus-visible:rf-ring-slate-950 focus-visible:rf-ring-offset-2 disabled:rf-pointer-events-none disabled:rf-opacity-50 data-[state=active]:rf-bg-white data-[state=active]:rf-text-slate-950 data-[state=active]:rf-shadow-sm dark:rf-ring-offset-slate-950 dark:focus-visible:rf-ring-slate-300 dark:data-[state=active]:rf-bg-slate-950 dark:data-[state=active]:rf-text-slate-50",
+      "rf-inline-flex rf-items-center rf-justify-center rf-whitespace-nowrap rf-rounded-md rf-px-3 rf-py-1.5 rf-text-sm rf-font-medium rf-ring-offset-white rf-transition-all focus-visible:rf-outline-none focus-visible:rf-ring-2 focus-visible:rf-ring-slate-950 focus-visible:rf-ring-offset-2 disabled:rf-pointer-events-none disabled:rf-opacity-50 data-[state=active]:rf-bg-white data-[state=active]:rf-text-slate-950 data-[state=active]:rf-shadow-sm dark:rf-ring-offset-slate-950 dark:focus-visible:rf-ring-slate-300 dark:data-[state=active]:rf-bg-slate-950 dark:data-[state=active]:rf-text-slate-50",
       className,
     )}
     {...props}

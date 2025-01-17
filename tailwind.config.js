48190c5cb42da45ec96304920aef70273c87e428
Arnav K <arnavkaushal09@gmail.com>
2025-01-18 02:10:14 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   prefix: "rf-",
-  darkMode: ["class", "class"],
+  darkMode: ["class"],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",

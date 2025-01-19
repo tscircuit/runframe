252420405011517e4143a07d056667dec4709fa2
seveibar <seveibar@gmail.com>
2025-01-18 16:50:12 -0800

    empty state for render log viewer, show progress even if not on render log page

--- lib/components/RenderLogViewer/RenderTimingsBar.tsx
+++ lib/components/RenderLogViewer/RenderTimingsBar.tsx
@@ -32,8 +32,8 @@ export const RenderTimingsBar = ({
                 backgroundColor: getPhaseColor(index),
               }}
             >
-              <div className="rf-opacity-0 rf-group-hover:opacity-100 rf-transition-opacity rf-absolute rf-bottom-full rf-left-1/2 rf-transform rf--translate-x-1/2 rf-mb-2 rf-px-2 rf-py-1 rf-text-xs rf-whitespace-nowrap rf-rounded rf-bg-gray-900 rf-text-white rf-pointer-events-none">
-                {phase}: {time}ms
+              <div className="rf-opacity-0 rf-group-hover:rf-opacity-100 rf-transition-opacity rf-absolute rf-bottom-full rf-left-1/2 rf-transform -rf-translate-x-1/2 rf-mb-2 rf-px-2 rf-py-1 rf-text-xs rf-whitespace-nowrap rf-rounded rf-bg-gray-900 rf-text-white rf-pointer-events-none">
+                {phase}: {time.toFixed(1)}ms
               </div>
             </div>
           )

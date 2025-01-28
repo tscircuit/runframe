7c908d838151c42192882991575ddf9c8ad84d6c
“mustafa” <mustafamulla765@gmail.com>
2025-01-28 17:05:40 +0530

    Apply formatting changes to RunFrameForCli.tsx

--- lib/components/RunFrameForCli/RunFrameForCli.tsx
+++ lib/components/RunFrameForCli/RunFrameForCli.tsx
@@ -4,14 +4,14 @@ import { SaveToSnippetsButton } from "./SaveToSnippetsButton"
 export const RunFrameForCli = (props: { debug?: boolean }) => {
   return (
     <div className="rf-h-screen rf-w-screen rf-flex rf-flex-col">
-    <RunFrameWithApi
-      debug={props.debug}
-      leftHeaderContent={
-        <div>
-          <SaveToSnippetsButton />
-        </div>
-      }
-    />
+      <RunFrameWithApi
+        debug={props.debug}
+        leftHeaderContent={
+          <div>
+            <SaveToSnippetsButton />
+          </div>
+        }
+      />
     </div>
   )
 }

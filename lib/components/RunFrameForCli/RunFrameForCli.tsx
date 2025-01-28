966fa15b717f149d2744cf7589ab47a81c7fef75
“mustafa” <mustafamulla765@gmail.com>
2025-01-28 16:01:25 +0530

    /fix/RunFrameForCli-full-viewport

--- lib/components/RunFrameForCli/RunFrameForCli.tsx
+++ lib/components/RunFrameForCli/RunFrameForCli.tsx
@@ -3,6 +3,7 @@ import { SaveToSnippetsButton } from "./SaveToSnippetsButton"
 
 export const RunFrameForCli = (props: { debug?: boolean }) => {
   return (
+    <div className="rf-h-screen rf-w-screen rf-flex rf-flex-col">
     <RunFrameWithApi
       debug={props.debug}
       leftHeaderContent={
@@ -11,5 +12,6 @@ export const RunFrameForCli = (props: { debug?: boolean }) => {
         </div>
       }
     />
+    </div>
   )
 }

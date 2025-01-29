60d7cb1e8a700b8abf90825ca16b86831bf83e63
“mustafa” <mustafamulla765@gmail.com>
2025-01-29 19:58:21 +0530

    Fixed formatting

--- lib/components/RunFrameForCli/RunFrameForCli.tsx
+++ lib/components/RunFrameForCli/RunFrameForCli.tsx
@@ -3,13 +3,13 @@ import { SaveToSnippetsButton } from "./SaveToSnippetsButton"
 
 export const RunFrameForCli = (props: { debug?: boolean }) => {
   return (
-      <RunFrameWithApi
-        debug={props.debug}
-        leftHeaderContent={
-          <div>
-            <SaveToSnippetsButton />
-          </div>
-        }
-      />
+    <RunFrameWithApi
+    debug={props.debug}
+    leftHeaderContent={
+      <div>
+        <SaveToSnippetsButton />
+      </div>
+    }
+  />
   )
 }

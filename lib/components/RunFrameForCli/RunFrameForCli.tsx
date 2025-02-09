0181855e62996126ab20210a7e431a36049b94bb
18bharathkumar <bharathms8055@gmail.com>
2025-02-09 14:57:20 +0530

    fixed viewport

--- lib/components/RunFrameForCli/RunFrameForCli.tsx
+++ lib/components/RunFrameForCli/RunFrameForCli.tsx
@@ -3,7 +3,7 @@ import { SaveToSnippetsButton } from "./SaveToSnippetsButton";
 
 export const RunFrameForCli = (props: { debug?: boolean }) => {
   return (
-    <div className="rf-w-full rf-h-full">
+    <div className="h-[100vh] rf-w-full">
       <RunFrameWithApi
         debug={props.debug}
         leftHeaderContent={

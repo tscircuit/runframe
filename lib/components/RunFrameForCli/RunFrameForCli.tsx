aea083adba094dbfcf0fd48d44ca58d5a3c7ffe5
18bharathkumar <bharathms8055@gmail.com>
2025-02-09 10:07:23 +0000

    fixed viewport

--- lib/components/RunFrameForCli/RunFrameForCli.tsx
+++ lib/components/RunFrameForCli/RunFrameForCli.tsx
@@ -1,15 +1,17 @@
-import { RunFrameWithApi } from "../RunFrameWithApi/RunFrameWithApi"
-import { SaveToSnippetsButton } from "./SaveToSnippetsButton"
+import { RunFrameWithApi } from "../RunFrameWithApi/RunFrameWithApi";
+import { SaveToSnippetsButton } from "./SaveToSnippetsButton";
 
 export const RunFrameForCli = (props: { debug?: boolean }) => {
   return (
-    <RunFrameWithApi
-      debug={props.debug}
-      leftHeaderContent={
-        <div>
-          <SaveToSnippetsButton />
-        </div>
-      }
-    />
-  )
-}
+    <div className="rf-w-full rf-h-full">
+      <RunFrameWithApi
+        debug={props.debug}
+        leftHeaderContent={
+          <div>
+            <SaveToSnippetsButton />
+          </div>
+        }
+      />
+    </div>
+  );
+};

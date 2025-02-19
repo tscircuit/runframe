e60e5d744a18e5524ee9d406a8e964fc52d70010
seveibar <seveibar@gmail.com>
2025-02-18 17:46:01 -0800

    allow standalone bundle to display cli version of runframe

--- lib/components/RunFrameWithApi/standalone.tsx
+++ lib/components/RunFrameWithApi/standalone.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from "react-dom/client"
 import { RunFrameWithApi } from "./RunFrameWithApi"
+import { RunFrameForCli } from "../RunFrameForCli/RunFrameForCli"
 
 const root = createRoot(document.getElementById("root")!)
 
-root.render(<RunFrameWithApi />)
+// @ts-ignore
+if (window.TSCIRCUIT_USE_RUNFRAME_FOR_CLI) {
+  root.render(<RunFrameForCli />)
+} else {
+  root.render(<RunFrameWithApi />)
+}

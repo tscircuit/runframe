8397098848f509aa7c87692ae1255e62242636d1
seveibar <seveibar@gmail.com>
2025-04-01 16:51:31 -0700

    add cad viewer

--- lib/preview.ts
+++ lib/preview.ts
@@ -1,5 +1,6 @@
 export * from "./components/CircuitJsonPreview/CircuitJsonPreview"
 export * from "./components/BomTable"
 export * from "./components/PcbViewerWithContainerHeight"
+export { CadViewer } from "@tscircuit/3d-viewer"
 export { PCBViewer as PcbViewer } from "@tscircuit/pcb-viewer"
 export { SchematicViewer } from "@tscircuit/schematic-viewer"

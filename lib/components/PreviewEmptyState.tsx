98956c04d54d7d86b61b4e062bcb165f392f1bca
Severin Ibarluzea <seveibar@gmail.com>
2025-04-25 17:10:00 -0700

    fix tips changing on rerender, improve tip text (#514)
    * fix tips changing on rerender, improve tip text
    
    * fix more tips
    
    * more random tips
    
    * support tags in string

--- lib/components/PreviewEmptyState.tsx
+++ lib/components/PreviewEmptyState.tsx
@@ -1,24 +1,29 @@
 import { Button } from "lib/components/ui/button"
-import { getRandomTipForUser } from "lib/utils/getRandomTipForUser "
+import { getRandomTipForUser } from "lib/utils/getRandomTipForUser"
 import { LightbulbIcon, PlayIcon } from "lucide-react"
+import { useMemo } from "react"
 
-const PreviewEmptyState = ({ onRunClicked }: { onRunClicked?: () => void }) => (
-  <div className="rf-flex rf-flex-col rf-items-center rf-justify-center rf-h-full rf-bg-gray-50 rf-text-gray-500 rf-p-4">
-    <LightbulbIcon className="rf-size-14 rf-mb-4" />
-    <p className="rf-text-md rf-break-words rf-max-w-xl rf-text-center">
-      {/* biome-ignore lint/security/noDangerouslySetInnerHtml: its internal function */}
-      Tip: <span dangerouslySetInnerHTML={{ __html: getRandomTipForUser() }} />
-    </p>
-    {onRunClicked && (
-      <Button
-        className="rf-mt-4 rf-bg-blue-600 rf-hover:bg-blue-500"
-        onClick={onRunClicked}
-      >
-        Run Code
-        <PlayIcon className="rf-w-3 rf-h-3 rf-ml-2" />
-      </Button>
-    )}
-  </div>
-)
+const PreviewEmptyState = ({ onRunClicked }: { onRunClicked?: () => void }) => {
+  const tipHtml = useMemo(() => getRandomTipForUser(), [])
+
+  return (
+    <div className="rf-flex rf-flex-col rf-items-center rf-justify-center rf-h-full rf-bg-gray-50 rf-text-gray-500 rf-p-4">
+      <LightbulbIcon className="rf-size-14 rf-mb-4" />
+      <p className="rf-text-md rf-break-words rf-max-w-xl rf-text-center">
+        {/* biome-ignore lint/security/noDangerouslySetInnerHtml: its internal function */}
+        Tip: <span dangerouslySetInnerHTML={{ __html: tipHtml }} />
+      </p>
+      {onRunClicked && (
+        <Button
+          className="rf-mt-4 rf-bg-blue-600 rf-hover:bg-blue-500"
+          onClick={onRunClicked}
+        >
+          Run Code
+          <PlayIcon className="rf-w-3 rf-h-3 rf-ml-2" />
+        </Button>
+      )}
+    </div>
+  )
+}
 
 export default PreviewEmptyState

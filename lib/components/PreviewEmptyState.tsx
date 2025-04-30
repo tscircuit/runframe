afabf21c59b61febedb8324704db06d3f968615f
mohan-bee <mohn08052006@gmail.com>
2025-05-01 02:17:09 +0530

    fix: rerender on tab change

--- lib/components/PreviewEmptyState.tsx
+++ lib/components/PreviewEmptyState.tsx
@@ -1,21 +1,16 @@
 import { Button } from "lib/components/ui/button"
 import { getRandomTipForUser } from "lib/utils/getRandomTipForUser"
 import { LightbulbIcon, PlayIcon } from "lucide-react"
-import { useEffect, useState } from "react"
+
+const storedTipHtml: string = getRandomTipForUser()
 
 const PreviewEmptyState = ({ onRunClicked }: { onRunClicked?: () => void }) => {
-  const [tipHtml, setTipHtml] = useState<string>("")
-
-  useEffect(() => {
-    setTipHtml(getRandomTipForUser())
-  }, [])
-
   return (
     <div className="rf-flex rf-flex-col rf-items-center rf-justify-center rf-h-full rf-bg-gray-50 rf-text-gray-500 rf-p-4">
       <LightbulbIcon className="rf-size-14 rf-mb-4" />
       <p className="rf-text-md rf-break-words rf-max-w-xl rf-text-center">
         {/* biome-ignore lint/security/noDangerouslySetInnerHtml: its internal function */}
-        Tip: <span dangerouslySetInnerHTML={{ __html: tipHtml }} />
+        Tip: <span dangerouslySetInnerHTML={{ __html: storedTipHtml }} />
       </p>
       {onRunClicked && (
         <Button

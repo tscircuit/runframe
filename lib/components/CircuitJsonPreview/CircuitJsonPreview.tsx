f062bf6188834addef509dff96c887fca715d7de
Arnav K <arnavkaushal09@gmail.com>
2025-05-13 20:50:05 +0530

    Update CircuitJsonPreview.tsx

--- lib/components/CircuitJsonPreview/CircuitJsonPreview.tsx
+++ lib/components/CircuitJsonPreview/CircuitJsonPreview.tsx
@@ -134,7 +134,7 @@ export const CircuitJsonPreview = ({
   }, [circuitJson])
 
   const setCadViewerRef = useCallback((value: Object3D) => {
-    let cadViewer;
+    let cadViewer
     if (cadViewer !== value) {
       window.TSCIRCUIT_3D_OBJECT_REF = value === null ? undefined : value
       cadViewer = value

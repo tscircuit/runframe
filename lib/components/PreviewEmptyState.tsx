dcb0fa209e30e70b9478a3693c193ce614ec2638
techmannih <manishchaudharyttt@gmail.com>
2025-05-17 10:54:49 +0530

    feat: Add loading state with dimmed tip and spinner

--- lib/components/PreviewEmptyState.tsx
+++ lib/components/PreviewEmptyState.tsx
@@ -1,14 +1,20 @@
 import { Button } from "lib/components/ui/button"
 import { getRandomTipForUser } from "lib/utils/getRandomTipForUser"
-import { LightbulbIcon, PlayIcon } from "lucide-react"
+import { Loader, LightbulbIcon, PlayIcon } from "lucide-react"
 
 const tipHtml: string = getRandomTipForUser()
 
-const PreviewEmptyState = ({ onRunClicked }: { onRunClicked?: () => void }) => {
+const PreviewEmptyState = ({ 
+  onRunClicked, 
+  isLoading 
+}: { 
+  onRunClicked?: () => void,
+  isLoading?: boolean 
+}) => {
   return (
     <div className="rf-flex rf-flex-col rf-items-center rf-justify-center rf-h-full rf-bg-gray-50 rf-text-gray-500 rf-p-4">
-      <LightbulbIcon className="rf-size-14 rf-mb-4" />
-      <p className="rf-text-md rf-break-words rf-max-w-xl rf-text-center">
+      <LightbulbIcon className={`rf-size-14 rf-mb-4 ${isLoading ? 'rf-opacity-50' : ''}`} />
+      <p className={`rf-text-md rf-break-words rf-max-w-xl rf-text-center ${isLoading ? 'rf-opacity-50' : ''}`}>
         {/* biome-ignore lint/security/noDangerouslySetInnerHtml: its internal function */}
         Tip: <span dangerouslySetInnerHTML={{ __html: tipHtml }} />
       </p>
@@ -16,9 +22,19 @@ const PreviewEmptyState = ({ onRunClicked }: { onRunClicked?: () => void }) => {
         <Button
           className="rf-mt-4 rf-bg-blue-600 rf-hover:bg-blue-500"
           onClick={onRunClicked}
+          disabled={isLoading}
         >
-          Run Code
-          <PlayIcon className="rf-w-3 rf-h-3 rf-ml-2" />
+          {isLoading ? (
+            <>
+              Loading
+              <Loader className="rf-w-3 rf-h-3 rf-ml-2 rf-animate-spin" />
+            </>
+          ) : (
+            <>
+              Run Code
+              <PlayIcon className="rf-w-3 rf-h-3 rf-ml-2" />
+            </>
+          )}
         </Button>
       )}
     </div>

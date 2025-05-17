834c747b366f44981a70d20fc47b78ab8f983314
techmannih <manishchaudharyttt@gmail.com>
2025-05-17 10:58:11 +0530

    format code

--- lib/components/PreviewEmptyState.tsx
+++ lib/components/PreviewEmptyState.tsx
@@ -4,17 +4,21 @@ import { Loader, LightbulbIcon, PlayIcon } from "lucide-react"
 
 const tipHtml: string = getRandomTipForUser()
 
-const PreviewEmptyState = ({ 
-  onRunClicked, 
-  isLoading 
-}: { 
-  onRunClicked?: () => void,
-  isLoading?: boolean 
+const PreviewEmptyState = ({
+  onRunClicked,
+  isLoading,
+}: {
+  onRunClicked?: () => void
+  isLoading?: boolean
 }) => {
   return (
     <div className="rf-flex rf-flex-col rf-items-center rf-justify-center rf-h-full rf-bg-gray-50 rf-text-gray-500 rf-p-4">
-      <LightbulbIcon className={`rf-size-14 rf-mb-4 ${isLoading ? 'rf-opacity-50' : ''}`} />
-      <p className={`rf-text-md rf-break-words rf-max-w-xl rf-text-center ${isLoading ? 'rf-opacity-50' : ''}`}>
+      <LightbulbIcon
+        className={`rf-size-14 rf-mb-4 ${isLoading ? "rf-opacity-50" : ""}`}
+      />
+      <p
+        className={`rf-text-md rf-break-words rf-max-w-xl rf-text-center ${isLoading ? "rf-opacity-50" : ""}`}
+      >
         {/* biome-ignore lint/security/noDangerouslySetInnerHtml: its internal function */}
         Tip: <span dangerouslySetInnerHTML={{ __html: tipHtml }} />
       </p>

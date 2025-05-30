712521d96284ba15f5be2ce07367d59855129596
techmannih <manishchaudharyttt@gmail.com>
2025-05-30 07:04:13 +0530

    update dependencies

--- examples/example06-render-logs.fixture.tsx
+++ examples/example06-render-logs.fixture.tsx
@@ -9,6 +9,14 @@ circuit.add(
 <board width="10mm" height="10mm">
   <resistor name="R1" resistance="1k" footprint="0402" />
   <capacitor name="C1" capacitance="1uF" footprint="0603" pcbX={4} />
+    <platedhole
+        shape="circular_hole_with_rect_pad"
+        holeDiameter={2}
+        rectPadWidth={4}
+        rectPadHeight={4}
+        pcbX={-4}
+        pcbY={0}
+      />
   <trace from=".R1 .pin1" to=".C1 .pin1" />
 </board>
 )

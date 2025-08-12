acf8e01ac85aa1f336ed735af7d10830fad56939
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-08-12 20:05:10 +0530

    style: add chevron icon to File menu trigger

--- lib/components/RunFrame/FileMenu.tsx
+++ lib/components/RunFrame/FileMenu.tsx
@@ -13,6 +13,7 @@ import {
   exportAndDownload,
 } from "lib/optional-features/exporting/export-and-download"
 import { useRunFrameStore } from "../RunFrameWithApi/store"
+import { ChevronRight } from "lucide-react"
 
 export const RunFrameFileMenu = () => {
   const circuitJson = useRunFrameStore((s) => s.circuitJson)
@@ -21,7 +22,8 @@ export const RunFrameFileMenu = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <div className="rf-inline-flex rf-items-center rf-justify-center rf-h-9 rf-rounded-lg rf-bg-zinc-100 rf-px-3 rf-mx-1 rf-text-sm rf-font-medium rf-text-zinc-500 dark:rf-bg-zinc-800 dark:rf-text-zinc-400 rf-whitespace-nowrap rf-cursor-pointer rf-relative">
-          File
+          <span>File</span>
+          <ChevronRight className="rf-ml-1 rf-h-4 rf-w-4" />
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent>

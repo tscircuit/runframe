f840475c1776eac43275b04fe3d7fea4570f7a62
Abimael Martell <abimex@gmail.com>
2025-08-18 22:50:10 -0700

    format

--- examples/example23-loading-state.fixture.tsx
+++ examples/example23-loading-state.fixture.tsx
@@ -9,12 +9,14 @@ export default () => {
     const timer = setTimeout(() => {
       setFsMap({
         "index.tsx": `
-export default () => (
-  <board width="10mm" height="10mm">
-    <resistor name="R1" resistance="1k" footprint="0402" />
-  </board>
-)
-        `.trim(),
+        // edit me
+        circuit.add(
+        <board width="10mm" height="10mm">
+          <resistor name="R1" resistance="1k" footprint="0402" />
+          <capacitor name="C1" capacitance="1uF" footprint="0603" pcbX={4} />
+          <trace from=".R1 .pin1" to=".C1 .pin1" />
+        </board>
+        )`.trim(),
       })
       setIsLoading(false)
     }, 2000)

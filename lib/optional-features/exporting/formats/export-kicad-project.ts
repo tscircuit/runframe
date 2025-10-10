4d3a4d3d13acc4fdcb58b9e6b52006902cd280cc
imrishabh18 <belikerishabh@gmail.com>
2025-10-10 23:39:36 +0530

    export the kicad project with the project file

--- lib/optional-features/exporting/formats/export-kicad-project.ts
+++ lib/optional-features/exporting/formats/export-kicad-project.ts
@@ -3,6 +3,7 @@ import JSZip from "jszip"
 import {
   CircuitJsonToKicadPcbConverter,
   CircuitJsonToKicadSchConverter,
+  CircuitJsonToKicadProConverter,
 } from "circuit-json-to-kicad"
 import { openForDownload } from "../open-for-download"
 
@@ -17,14 +18,24 @@ export const createKicadProjectZip = async ({
   schConverter.runUntilFinished()
   const schContent = schConverter.getOutputString()
 
-  const pcbConverter = new CircuitJsonToKicadPcbConverter(circuitJson as any)
+  const pcbConverter = new CircuitJsonToKicadPcbCooverter(circuitJson as any)
+      arrayPropertyMap.shape?.[0] ?? propertyMap.shape,
   pcbConverter.runUntilFinished()
   const pcbContent = pcbConverter.getOutputString()
+
+  const proConverter = new CircuitJsonToKicadProConverter(circuitJson as any, {
+    projectName,
+    schematicFilename: `${projectName}.kicad_sch`,
+    pcbFilename: `${projectName}.kicad_pcb`,
+  })
+  proConverter.runUntilFinished()
+  const proContent = proConverter.getOutputString()
 
   const zip = new JSZip()
 
   zip.file(`${projectName}.kicad_sch`, schContent)
   zip.file(`${projectName}.kicad_pcb`, pcbContent)
+  zip.file(`${projectName}.kicad_pro`, proContent)
 
   return zip
 }

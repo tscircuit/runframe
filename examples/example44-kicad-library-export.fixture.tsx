fd9fd0ccea0ed27fede0b2ed0af498bed71ad933
mustafa <mustafamulla765@gmail.com>
2025-12-20 17:48:31 +0530

    update

--- examples/example44-kicad-library-export.fixture.tsx
+++ examples/example44-kicad-library-export.fixture.tsx
@@ -19,11 +19,12 @@ export default () => (
       name="SW1"
       footprint="pushbutton"
       pcbX={5}
-      cadModel={{
-        modelUrl: stepUrl,
-        rotationOffset: { x: 0, y: 0, z: 270 },
-        positionOffset: { x: 0.5, y: -0.3, z: 0 },
-      }}
+      cadModel={
+        <cadmodel
+          modelUrl={stepUrl}
+          rotationOffset={ x: 0, y: 0, z: 270 },
+          positionOffset={ x: 0.5, y: -0.3, z: 0 } />
+      }
     />
     <trace from=".R1 .pin2" to=".C1 .pin1" />
   </board>

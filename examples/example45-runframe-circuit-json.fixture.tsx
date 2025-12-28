a545526196c3de20d09ca4219f5f371a5ab03c3a
Ayush Jhawar <ayushjhawar499@gmail.com>
2025-12-28 22:47:00 +0530

    refactor

--- examples/example45-runframe-circuit-json.fixture.tsx
+++ examples/example45-runframe-circuit-json.fixture.tsx
@@ -5,38 +5,13 @@ const circuitJsonContent = await fetch(
   "https://raw.githubusercontent.com/tscircuit/circuitjson.com/94fa2b7635fd82b5e9b54de13b5a2f11b58b0611/assets/usb-c-flashlight.json",
 ).then((r) => r.text())
 
-const tsxContent = `export default () => (
-  <board>
-    <resistor resistance="1k" footprint="0402" name="R1" />
-  </board>
-)`
-
 export const WithCircuitJsonFile = () => {
-  const fsMap = new Map([
-    ["test.circuit.json", circuitJsonContent],
-    ["index.tsx", tsxContent],
-  ])
+  const fsMap = new Map([["flashlight.circuit.json", circuitJsonContent]])
 
   return (
     <RunFrame
       fsMap={fsMap}
-      mainComponentPath="test.circuit.json"
-      defaultToFullScreen={false}
-      showToggleFullScreen={true}
-    />
-  )
-}
-
-export const WithTsxFile = () => {
-  const fsMap = new Map([
-    ["test.circuit.json", circuitJsonContent],
-    ["index.tsx", tsxContent],
-  ])
-
-  return (
-    <RunFrame
-      fsMap={fsMap}
-      mainComponentPath="index.tsx"
+      mainComponentPath="flashlight.circuit.json"
       defaultToFullScreen={false}
       showToggleFullScreen={true}
     />
@@ -45,5 +20,4 @@ export const WithTsxFile = () => {
 
 export default {
   WithCircuitJsonFile,
-  WithTsxFile,
 }

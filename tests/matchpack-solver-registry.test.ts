0a91730667b2c2ea8f62c140aaf294c060d10792
mohan-bee <mohn08052006@gmail.com>
2026-08-09 01:50:05 +0530

    up

--- tests/matchpack-solver-registry.test.ts
+++ tests/matchpack-solver-registry.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test"
-import { SOLVERS } from "@tscircuit/core"
+import { SOLVERS } from "tscircuit"
 
 test("includes Matchpack in the solver registry", () => {
   expect(SOLVERS.LayoutPipelineSolver).toBeDefined()

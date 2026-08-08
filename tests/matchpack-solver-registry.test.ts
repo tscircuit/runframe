8392eb084841c6b5fbb20f7906f26a5c70a48fa3
Mohan <mohn08052006@gmail.com>
2026-08-09 02:18:08 +0530

    Show Matchpack in the Solvers tab (#4403)
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up

---
+++ tests/matchpack-solver-registry.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "bun:test"
+import { SOLVERS } from "@tscircuit/core"
+
+test("includes Matchpack in the solver registry", () => {
+  expect(SOLVERS.LayoutPipelineSolver).toBeDefined()
+})

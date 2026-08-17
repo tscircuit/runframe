73f2f5313ea7e371cb176f287686e93e62b7d056
mohan-bee <mohn08052006@gmail.com>
2026-08-17 12:09:24 +0530

    up

--- examples/example58-autolayout-section.fixture.tsx
+++ examples/example58-autolayout-section.fixture.tsx
@@ -7,38 +7,47 @@ export default () => (
       "main.tsx": `
 circuit.add(
   <board width="20mm" height="10mm" routingDisabled>
-    <schematicsection name="power" displayName="Power" />
-    <schematicsection name="control" displayName="Control" />
+    <schematicsection name="decoupling" displayName="Decoupling" />
+    <schematicsection name="indicator" displayName="Status Indicator" />
 
-    <resistor
-      name="R1"
-      resistance="1k"
-      footprint="0402"
-      pcbX={-6}
-      schSectionName="power"
-    />
     <capacitor
       name="C1"
       capacitance="1uF"
       footprint="0402"
-      pcbX={-2}
-      schSectionName="power"
-    />
-
-    <resistor
-      name="R2"
-      resistance="10k"
-      footprint="0402"
-      pcbX={2}
-      schSectionName="control"
+      pcbX={-6}
+      schSectionName="decoupling"
     />
     <capacitor
       name="C2"
       capacitance="100nF"
       footprint="0402"
-      pcbX={6}
-      schSectionName="control"
+      pcbX={-2}
+      schSectionName="decoupling"
     />
+
+    <resistor
+      name="R1"
+      resistance="1k"
+      footprint="0402"
+      pcbX={2}
+      schSectionName="indicator"
+    />
+    <led
+      name="D1"
+      color="green"
+      footprint="0603"
+      pcbX={6}
+      schSectionName="indicator"
+    />
+
+    <trace from={"net.VCC"} to={".C1 > .pin1"} />
+    <trace from={".C1 > .pin2"} to={"net.GND"} />
+    <trace from={"net.VCC"} to={".C2 > .pin1"} />
+    <trace from={".C2 > .pin2"} to={"net.GND"} />
+
+    <trace from={"net.VCC"} to={".R1 > .pin1"} />
+    <trace from={".R1 > .pin2"} to={".D1 > .pin1"} />
+    <trace from={".D1 > .pin2"} to={"net.GND"} />
   </board>,
 )
 `,
